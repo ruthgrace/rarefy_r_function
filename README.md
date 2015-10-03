7126e01ab72d26a8fd3e4b7927d34c3368d7b800
ruthgrace <ruthgracewong@gmail.com>
2015-10-03 14:11:56 -0400

    added rarefy function code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rarefy_r_function
 Simple rarefaction function in R, with or without replacement
+
+The function is run like
+
+```R
+rarefied.counts <- rarefy(features, counts, samples, replacement)
+```
+where
+
+* features is a vector of features (OTU names, gene names, etc.)
+* counts is a vector of counts for each feature (must be in the same order as features)
+* samples is the number of samples to rarefy to (must be <= sum(counts) for rarefaction without replacement)
+* replacement is whether or not sampling should be done with replacement or without (true/false)
+
+The output is a vector of counts where each count corresponds with the features.
---
+++ rarefy_function.r
@@ -0,0 +1,23 @@
+# features is a vector of features (OTU names, gene names, etc.)
+# counts is a vector of counts for each feature
+# samples is the number of samples to rarefy to (must be <= sum(counts) for rarefaction without replacement)
+# replacement is whether or not sampling should be done with replacement or without (true/false)
+rarefy <- function(features, counts, samples, replacement) {
+  # pool has the name of each OTU duplicated as many times as the count of that OTU
+  counts.no.prior <- counts
+  counts.no.prior[counts.no.prior==prior] <- 0
+  pool <- c(1:sum(counts.no.prior))
+  index <- 0
+  for (i in c(1:length(features))) {
+    if(counts.no.prior[i] >= 1) {
+      pool[(index+1):(index+counts.no.prior[i])] <- rep(features[i],counts.no.prior[i])
+      index <- index + counts.no.prior[i]
+    }
+  }
+  sample.pool <- sample(pool, samples, replace=replacement)
+  newcounts <- counts.no.prior
+  newcounts[] <- 0
+  aggregate.pool <- table(sample.pool)
+  newcounts[match(dimnames(aggregate.pool)[[1]],features)] <- aggregate.pool
+  return(newcounts)
+}
